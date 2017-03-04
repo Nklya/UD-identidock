b4b8a175d7d86afb3d7c48d45a860ea42b1b79bb
Nklya <nklya@hotmail.com>
2017-03-04 12:14:48 +0300

    Changed app

--- app/identidock.py
+++ app/identidock.py
@@ -2,11 +2,24 @@
 
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+default_name = 'Hu Cho'
+
 
 @app.route('/')
-def hello_world():
-  return 'Hello World!!!\n'
+def get_identicon():
+    name = default_name
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+    Hello <input type="text" name="name" value="{}">
+    <input type="submit"
+    </form>
+    <p>You look like a:
+    <img src="/monster/monster.png"/>
+    '''.format(name)
+    footer = '</body></html>'
+    return header + body + footer
+
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
